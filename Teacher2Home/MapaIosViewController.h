1b07b5bc31898ec8b64875ef717d2e2163cc64ad
Luis Soler <lsoler@MacBook-Pro-Developer.local>
2015-05-08 12:36:16 -0500

    Ok

---
+++ Teacher2Home/MapaIosViewController.h
@@ -0,0 +1,13 @@
+//
+//  MapaIosViewController.h
+//  Teacher2Home
+//
+//  Created by Luis Soler on 8/05/15.
+//  Copyright (c) 2015 Luis Soler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapaIosViewController : UIViewController
+
+@end
---
+++ Teacher2Home/MapaIosViewController.m
@@ -0,0 +1,37 @@
+//
+//  MapaIosViewController.m
+//  Teacher2Home
+//
+//  Created by Luis Soler on 8/05/15.
+//  Copyright (c) 2015 Luis Soler. All rights reserved.
+//
+
+#import "MapaIosViewController.h"
+
+@interface MapaIosViewController ()
+
+@end
+
+@implementation MapaIosViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
